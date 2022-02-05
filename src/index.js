387de9fcf9f977f8c5bdae3dbe2c9ec52de505ef
qurias <qurias_it@mail.ru>
2022-02-05 16:53:48 +0300

    add mini redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Route } from 'react-router-dom'
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 
 import './scss/app.scss';
-
 import App from './App';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Route>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Route>
   </React.StrictMode>,
   document.getElementById('root')
